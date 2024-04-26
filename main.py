df65d196209ff3bb1440465fc7d028c8299ca0cc
Aransu0 <MustafaMourad25@gmail.com>
2024-04-25 23:00:11 -0400

    Update main.py

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ def get_directory():
     file = tk.filedialog.askopenfile(mode="r")
     if file:
         try:
+            # Check if an image is already displayed
             filepath
         except:
+            # If not, do not replace
             replace = False
         else:
-            global replace
+            # If so, replace the image
             replace = True
         finally:
+            # Get directory of selected file and display it
             filepath = os.path.abspath(file.name)
             display_image(filepath, replace)
 
@@ -33,26 +36,31 @@ def display_image(directory, replace):
     global image_label
 
     if replace == True:
+        # If an image is already displayed, remove it
         image_label.pack_forget()
 
+    # Get image from file
     image = Image.open(directory)
 
+    # Resize images to a fixed height
     aspect_ratio = image.width / image.height
     new_width = int(650 * aspect_ratio)
     resized = image.resize((new_width, 650), Image.Resampling.LANCZOS)
 
+    # Replace old image with the resized version
     new_image = ImageTk.PhotoImage(resized)
 
+    # Display the image
     image_label = tk.Label(root, image=new_image)
     image_label.image = new_image
     image_label.pack()
 
 
-# Add a Label widget
+# Add a label widget
 label = tk.Label(root, text="Click the Button to browse the Files")
 label.pack(pady=10)
 
-# Create a Button
+# Create a button
 button = tk.Button(root, text="Browse", command=get_directory).pack(pady=20)
 
 root.mainloop()
